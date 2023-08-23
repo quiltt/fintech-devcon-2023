6e5acad9f879548d0bcaa805390ba3aee0591cf2
Zane Wolfgang Pickett <sirwolfgang@users.noreply.github.com>
2023-08-23 11:34:05 -0500

    Setup Sandbox connectors

--- src/components/buttons/MxButton.tsx
+++ src/components/buttons/MxButton.tsx
@@ -1,11 +1,19 @@
 'use client'
 
 import { Button } from './Button'
-import { QuilttButton } from '@quiltt/react'
+import { QuilttButton, useQuilttSession } from '@quiltt/react'
+
+const CONNECTORS: { [eid: string]: string } = {
+  env_16dfkSTsGxQn1YaJ8h6SzLm: 's4baburzo2', // Production
+  env_12t6FoVKzfqi99sWisEKQL: '54tnmmn7eg', // Sandbox
+}
 
 export const MxButton = () => {
+  const { session } = useQuilttSession()
+  if (!session) return
+
   return (
-    <QuilttButton connectorId="s4baburzo2" as={Button}>
+    <QuilttButton connectorId={CONNECTORS[session.claims.eid]} as={Button}>
       Launch MX Connect
     </QuilttButton>
   )
--- src/components/buttons/PlaidButton.tsx
+++ src/components/buttons/PlaidButton.tsx
@@ -1,11 +1,21 @@
 'use client'
 
 import { Button } from './Button'
-import { QuilttButton } from '@quiltt/react'
+import { QuilttButton, useQuilttSession } from '@quiltt/react'
+
+const CONNECTORS: { [eid: string]: string } = {
+  env_16dfkSTsGxQn1YaJ8h6SzLm: '2he3hw6g5d', // Production
+  env_12t6FoVKzfqi99sWisEKQL: 'jse7ksai7n', // Sandbox
+}
 
 export const PlaidButton = () => {
+  const { session } = useQuilttSession()
+  if (!session) return
+
+  console.log(session.claims.eid)
+
   return (
-    <QuilttButton connectorId="2he3hw6g5d" as={Button}>
+    <QuilttButton connectorId={CONNECTORS[session.claims.eid]} as={Button}>
       Launch Plaid Link
     </QuilttButton>
   )
